ba6f13e9f2e7e8676f3aaba837af77c642d78edc
Warren G Brown <wgoff194@gmail.com>
2019-01-20 07:07:01 -0500

    Update wallboard.py

--- lib/wallboard.py
+++ lib/wallboard.py
@@ -16,34 +16,40 @@ from bs4 import BeautifulSoup
 
 ###
 # Functions
-### 
+###
 
 def active_users(sc,sl_channel,user_in):
     """
     """
     user_in = ''
     sl_mem_codes= sc.api_call("conversations.members",channel=sl_channel)['members']
+    for memcode in sl_mem_codes :
+        userdata = sc.api_call("users.info",user=memcode)['user']['name']
+        json_url = "https://wallboard.supportdev.liquidweb.com/api/data/agents/" + userdata
+        try:
+            conni = urllib.request.urlopen(json_url)
+        except urllib.error.HTTPError as e:
+            ecode = 'HTTPError: {}'.format(e.code)
+        except urllib.error.URLError as e:
+            ecode = 'URLError: {}'.format(e.reason)
+        else:
+            with urllib.request.urlopen(json_url) as url:
+                json_data = json.loads(url.read().decode())
+                if 'punched' in json_data:
+                    user_in = user_in + ' @'+userdata
+    return user_in
+
+def wallchk():
+    """
+    """
+    wall_chk = 0
     json_url = "https://wallboard.supportdev.liquidweb.com/api/data/agents/"
     try:
         conni = urllib.request.urlopen(json_url)
     except urllib.error.HTTPError as e:
-        user_in = '@here'
+        wall_chk = '2'
     except urllib.error.URLError as e:
-        user_in = '@here'
+        wall_chk = '2'
     else:
-        for memcode in sl_mem_codes :
-            userdata = sc.api_call("users.info",user=memcode)['user']['name']
-            json_url = "https://wallboard.supportdev.liquidweb.com/api/data/agents/" + userdata
-            try:
-                conni = urllib.request.urlopen(json_url)
-            except urllib.error.HTTPError as e:
-                 ecode = 'HTTPError: {}'.format(e.code)
-            except urllib.error.URLError as e:
-                 ecode = 'URLError: {}'.format(e.reason)
-            else:
-                with urllib.request.urlopen(json_url) as url:
-                    json_data = json.loads(url.read().decode())
-                    if 'punched' in json_data:
-                        user_in = user_in + ' @'+userdata
-    return user_in
-
+        wall_chk = '1'
+    return wall_chk
